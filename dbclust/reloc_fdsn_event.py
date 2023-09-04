bf92640c4c18b73e2ffea23483d7e5d621e5fe93
Marc Grunberg <marc.grunberg@unistra.fr>
2023-09-05 01:07:04 +0200

    add fdsn profile, velocity profile, cut off distance, loglevel

--- dbclust/reloc_fdsn_event.py
+++ dbclust/reloc_fdsn_event.py
@@ -39,9 +39,25 @@ if __name__ == "__main__":
         "-p",
         "--profile",
         default=None,
-        dest="profile_name",
-        help="profile name to use",
+        dest="velocity_profile_name",
+        help="velocity profile name to use",
         type=str,
+    )
+    parser.add_argument(
+        "-f",
+        "--fdsn-profile",
+        default=None,
+        dest="fdsn_profile",
+        help="fdsn profile",
+        type=str,
+    )
+    parser.add_argument(
+        "-d",
+        "--dist-km-cutoff",
+        default=None,
+        dest="dist_km_cutoff",
+        help="cut off distance in km",
+        type=float,
     )
     parser.add_argument(
         "-e",
@@ -51,26 +67,56 @@ if __name__ == "__main__":
         help="event id",
         type=str,
     )
+    parser.add_argument(
+        "-l",
+        "--loglevel",
+        default="INFO",
+        dest="loglevel",
+        help="loglevel (debug,warning,info,error)",
+        type=str,
+    )
 
     args = parser.parse_args()
     if not args.profile_conf_file or not args.event_id:
         parser.print_help()
         sys.exit()
 
+    numeric_level = getattr(logging, args.loglevel.upper(), None)
+    if not numeric_level:
+        logger.error("Invalid loglevel '%s' !", args.loglevel.upper())
+        logger.error("loglevel should be: debug,warning,info,error.")
+        sys.exit(255)
+    else:
+        logger.setLevel(numeric_level)
+
     conf = load_config(args.profile_conf_file)
     if not conf:
         sys.exit()
 
+    
     nll_conf = conf["nll"]
     parameters_conf = conf["parameters"]
     fdsnws_conf = conf["fdsnws"]
-    profile_conf = conf["profile"]
-    if hasattr(args, "profile_name") and args.profile_name:
-        default_profile = args.profile_name
+    velocity_profile_conf = conf["velocity_profile"]
+    if hasattr(args, "velocity_profile_name") and args.velocity_profile_name:
+        default_velocity_profile = args.velocity_profile_name
     else:
-        default_profile = conf["default_profile"]
-    logger.info(f"Using {default_profile} profile")
+        default_velocity_profile = conf["default_velocity_profile"]
+    logger.info(f"Using {default_velocity_profile} profile")
     quakeml_conf = conf["quakeml"]
+
+    # force fdsn ws
+    fdsnws_cfg = conf["fdsnws"]
+    if not args.fdsn_profile:
+        default_url_mapping = fdsnws_cfg["default_url_mapping"]
+    else:
+        default_url_mapping = args.fdsn_profile
+    fdsn_debug = fdsnws_cfg["fdsn_debug"]
+    url_mapping = fdsnws_cfg["url_mapping"]
+    if default_url_mapping not in fdsnws_cfg["url_mapping"]:
+        logger.error("unknown fdsn profile '%s'. Exiting !", default_url_mapping)
+        sys.exit(255)
+    ws_event_url = url_mapping[default_url_mapping]["ws_event_url"]
 
     verbose = conf["verbose"]
     tmpdir = conf["tmpdir"]
@@ -83,7 +129,10 @@ if __name__ == "__main__":
     S_uncertainty = parameters_conf["S_uncertainty"]
     P_time_residual_threshold = parameters_conf["P_time_residual_threshold"]
     S_time_residual_threshold = parameters_conf["S_time_residual_threshold"]
-    dist_km_cutoff  = parameters_conf["dist_km_cutoff"]
+    if not args.dist_km_cutoff:
+        dist_km_cutoff  = parameters_conf["dist_km_cutoff"]
+    else:
+        dist_km_cutoff  = args.dist_km_cutoff
     # use_deactivated_arrivals = parameters_conf["use_deactivated_arrivals"]
 
     # NonLinLoc
@@ -91,11 +140,11 @@ if __name__ == "__main__":
     nlloc_times_path = nll_conf["times_path"]
     nlloc_template_path = nll_conf["template_path"]
     template = None
-    for p in profile_conf:
-        if p["name"] == default_profile:
+    for p in velocity_profile_conf:
+        if p["name"] == default_velocity_profile:
             template = p["template"]
     if not template:
-        logger.error(f"profile {default_profile} does not exist !")
+        logger.error(f"profile {default_velocity_profile} does not exist !")
         sys.exit()
     nlloc_template = os.path.join(nlloc_template_path, template)
     nlloc_verbose = nll_conf["verbose"]
@@ -107,10 +156,8 @@ if __name__ == "__main__":
         "author": quakeml_conf["author"],
         "evaluation_mode": quakeml_conf["evaluation_mode"],
         "method_id": quakeml_conf["method_id"],
-        "model_id": default_profile,
+        "model_id": default_velocity_profile,
     }
-
-    fdsnws = fdsnws_conf["event"]
 
     if tmpdir is None:
         tmpdir = tempfile.TemporaryDirectory()
@@ -135,9 +182,11 @@ if __name__ == "__main__":
         #
         quakeml_settings=quakeml_settings,
         nll_verbose=nlloc_verbose,
+        #
+        log_level=numeric_level,
     )
 
-    cat = reloc_fdsn_event(locator, args.event_id, fdsnws)
+    cat = reloc_fdsn_event(locator, args.event_id, ws_event_url)
     if tmpdir is None:
         tmpdir.cleanup()
 
