7c83b5835b496bf5028d4d35a8aeb3c7fc34920d
Marc Grunberg <marc.grunberg@unistra.fr>
2024-01-02 01:08:46 +0100

    filter by station and sort by phase, as the order is kept by groupby

--- Utils/pick_preproc.py
+++ Utils/pick_preproc.py
@@ -11,7 +11,7 @@ from sklearn.cluster import DBSCAN
 def unload_too_close_picks_clustering(
     csv_file_in, csv_file_out, P_delta_time, S_delta_time
 ):
-    print(f"Reading from {csv_file_out}.")
+    print(f"Reading from {csv_file_in}.")
     df = pd.read_csv(csv_file_in)
     df.rename(
         columns={
@@ -34,32 +34,38 @@ def unload_too_close_picks_clustering(
     # run separately by phase type
     for phase in ("P", "S"):
         print(f"Working on {phase}.")
+        phase_df = df[df["phase_type"].str.contains(phase)].copy()
+        phase_df["phase_time"] = pd.to_datetime(phase_df["phase_time"], utc=True)
+
+        # create numeric_time for time distance computation
+        min_timestamp = phase_df["phase_time"].min()
+        phase_df["numeric_time"] = (
+            phase_df["phase_time"] - pd.to_datetime(min_timestamp, utc=True)
+        ).dt.total_seconds()
 
         # dbscan configuration
         if "P" in phase:
             max_distance = P_delta_time  # secondes
         else:
             max_distance = S_delta_time  # secondes
-
         min_samples = 1
         dbscan = DBSCAN(eps=max_distance, min_samples=min_samples, metric="euclidean")
-
-        phase_df = df[df["phase_type"].str.contains(phase)].copy()
-        phase_df["phase_time"] = pd.to_datetime(phase_df["phase_time"], utc=True)
-        # get min time from df
-        min_timestamp = phase_df["phase_time"].min()
-        # create numeric_time for time distance computation
-        phase_df["numeric_time"] = (
-            phase_df["phase_time"] - pd.to_datetime(min_timestamp, utc=True)
-        ).dt.total_seconds()
 
         # loop over station_id
         for station_id in phase_df["station_id"].drop_duplicates():
             print(f"Working on {phase}/{station_id}")
-            tmp_df = phase_df.loc[phase_df["station_id"] == station_id].copy()
+            # filter by station and sort by phase, as the order is kept by groupby
+            # The order should be Pn, Pg, P.
+            tmp_df = (
+                phase_df.loc[phase_df["station_id"] == station_id]
+                .sort_values(by="phase_type", ascending=False)
+                .copy()
+            )
             before = len(tmp_df)
+
             # clusterize by station_id
             tmp_df["cluster"] = dbscan.fit_predict(tmp_df[["numeric_time"]])
+
             # keeps only the pick with the higher score
             idx = tmp_df.groupby(["cluster"])["phase_score"].idxmax()
             tmp_df = tmp_df.loc[idx]
@@ -76,6 +82,18 @@ def unload_too_close_picks_clustering(
     )
     print(results)
     print(f"Writing to {csv_file_out}.")
+
+    df.rename(
+        columns={
+            "station_id": "seedid",
+            "phase_type": "phasename",
+            "phase_time": "time",
+            "phase_score": "probability",
+        },
+        inplace=True,
+    )
+
+    results.sort_values(by=["phase_time", "station_id"], inplace=True)
     results.to_csv(csv_file_out, index=False)
 
 
