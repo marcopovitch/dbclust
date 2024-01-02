d65697d8620bf79a4ced6be4936b512c9457614e
Marc Grunberg <marc.grunberg@unistra.fr>
2024-01-02 01:15:37 +0100

    set refinement_iterations to default

--- dbclust/dbclust2pyocto.py
+++ dbclust/dbclust2pyocto.py
@@ -63,9 +63,9 @@ def dbclust2pyocto(myclust, associator_cfg, velocity_model, min_com_phases):
             lat=(min_lat, max_lat),
             lon=(min_lon, max_lon),
             zlim=associator_cfg["zlim"],
-            time_before=associator_cfg["time_before"],  # should be greater than dbclust time_window parameter
-            location_split_depth=6,
-            location_split_return=4,
+            time_before=associator_cfg[
+                "time_before"
+            ],  # should be greater than dbclust time_window parameter
             max_pick_overlap=associator_cfg["max_pick_overlap"],
             min_pick_fraction=associator_cfg["min_pick_fraction"],
             min_node_size=associator_cfg["min_node_size"],  # default 10
@@ -79,7 +79,9 @@ def dbclust2pyocto(myclust, associator_cfg, velocity_model, min_com_phases):
             n_s_picks=associator_cfg["n_s_picks"],
             n_p_and_s_picks=associator_cfg["n_p_and_s_picks"],
             exponential_edt=True,
-            # refinement_iterations=5,
+            location_split_depth=6,  # default 6
+            location_split_return=4,  # default 4
+            refinement_iterations=3,  # default 3
         )
         associator.transform_stations(stations)
 
