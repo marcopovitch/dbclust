6d39bd774636208509b035f145c6344ed4b3f085
Marc Grunberg <marc.grunberg@unistra.fr>
2024-01-02 01:12:09 +0100

    export preloc to pref origin distance

--- Utils/picks_stats.py
+++ Utils/picks_stats.py
@@ -8,13 +8,13 @@ from obspy import read_events, Catalog, read, UTCDateTime, read_inventory
 
 
 def get_pick_proba_info(outfile, cat, show_station_name=False):
-    with open(outfile, 'w') as f:
+    with open(outfile, "w") as f:
         # csv header
         if show_station_name:
             f.write("station_id,phase_name,probability,source\n")
         else:
             f.write("phase_name,probability,source\n")
-            
+
         for event in cat:
             origin = event.preferred_origin()
             for arrival in origin.arrivals:
@@ -30,14 +30,30 @@ def get_pick_proba_info(outfile, cat, show_station_name=False):
                         info = json.loads(comment.text)
                         if "probability" in info.keys():
                             if show_station_name:
-                                f.write("%s,%s,%.2f,%s\n" % (pick.waveform_id.get_seed_string()[:-1], pick.phase_hint, info["probability"]["value"], info["probability"]["name"]))
+                                f.write(
+                                    "%s,%s,%.2f,%s\n"
+                                    % (
+                                        pick.waveform_id.get_seed_string()[:-1],
+                                        pick.phase_hint,
+                                        info["probability"]["value"],
+                                        info["probability"]["name"],
+                                    )
+                                )
                             else:
-                                f.write("%s,%.2f,%s\n" % (pick.phase_hint, info["probability"]["value"], info["probability"]["name"]))
+                                f.write(
+                                    "%s,%.2f,%s\n"
+                                    % (
+                                        pick.phase_hint,
+                                        info["probability"]["value"],
+                                        info["probability"]["name"],
+                                    )
+                                )
                     except:
                         pass
 
+
 def get_event_ids_info(outfile, cat):
-    with open(outfile, 'w') as f:
+    with open(outfile, "w") as f:
         f.write("time,event_id,nb_agencies,agencies_list\n")
         for event in sorted(cat.events, key=lambda e: e.preferred_origin().time):
             origin = event.preferred_origin()
@@ -52,7 +68,25 @@ def get_event_ids_info(outfile, cat):
                     ids.extend(info["event_ids"])
             all_ids = " ".join(ids)
             f.write(f"{event.resource_id.id},{len(ids)},{all_ids}\n")
-        
+
+
+def get_distance_km_info(outfile, cat):
+    with open(outfile, "w") as f:
+        f.write("time,event_id,dist_km,event_type\n")
+        for event in sorted(cat.events, key=lambda e: e.preferred_origin().time):
+            origin = event.preferred_origin()
+            f.write(f"{origin.time},")
+            dist_km = None
+            for comment in event.comments:
+                try:
+                    info = json.loads(comment.text)
+                except:
+                    continue
+                if "preloc_distance_km" in info.keys():
+                    dist_km = info["preloc_distance_km"]
+                    break
+            f.write(f"{event.resource_id.id},{dist_km},{event.event_type}\n")
+
 
 if __name__ == "__main__":
     # default logger
@@ -86,7 +120,7 @@ if __name__ == "__main__":
         type=str,
     )
     args = parser.parse_args()
-    
+
     if not args.inputfile or not args.outputfile:
         parser.print_help()
         sys.exit(255)
@@ -101,8 +135,8 @@ if __name__ == "__main__":
         logger.error("loglevel should be: debug,warning,info,error.")
         sys.exit(255)
     logger.setLevel(numeric_level)
-    
+
     cat = read_events(args.inputfile)
     get_pick_proba_info(args.outputfile, cat, show_station_name=True)
     get_event_ids_info("events_merge_info.csv", cat)
-    
+    get_distance_km_info("preloc_distance.csv", cat)
