2e13024dc8da61b73bfbb646b599ab605b31c556
Marc Grunberg <marc.grunberg@unistra.fr>
2024-02-11 23:44:35 +0100

    change pick type to csv or parquet

--- dbclust/db.py
+++ dbclust/db.py
@@ -1,14 +1,47 @@
 #!/usr/bin/env python
+from typing import Optional
+
 import duckdb
 
 
-def duckdb_init(parquet_filename: str):
-    duckdb_con = duckdb.connect(database=":memory:")
+def duckdb_init(filename: str, type: str):
+    if type == "parquet":
+        conn = duckdb_init_parquet(filename)
+        return conn
+    else:
+        conn = duckdb_init_csv(filename)
+    return conn
+
+
+def duckdb_init_parquet(parquet_filename: str, threads: int = 4):
+    config = {"threads": threads}
+
+    duckdb_con = duckdb.connect(
+        database=":memory:",
+        config=config,
+    )
     try:
         duckdb_con.execute(
             f"CREATE VIEW PICKS AS SELECT * "
             f"FROM parquet_scan('{parquet_filename}');"
         )
+
+    except Exception as e:
+        raise e
+
+    return duckdb_con
+
+
+def duckdb_init_csv(filename: str, threads: int = 1):
+    config = {"threads": threads}
+
+    duckdb_con = duckdb.connect(
+        database=":memory:",
+        config=config,
+    )
+    try:
+        duckdb_con.execute(f"CREATE VIEW PICKS AS SELECT * FROM '{filename}';")
+
     except Exception as e:
         raise e
 
