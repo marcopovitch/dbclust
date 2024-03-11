301e02fe21607b4ae9f8cc280957ac9e3c4f28ad
Marc Grunberg <marc.grunberg@unistra.fr>
2024-03-11 11:14:11 +0100

    picks selection uses pick evaluation to prioritize manual picks

--- Utils/pick_preproc.py
+++ Utils/pick_preproc.py
@@ -7,24 +7,73 @@ from datetime import timedelta
 
 import pandas as pd
 from icecream import ic
+from pandas.core.groupby import GroupBy
 from sklearn.cluster import DBSCAN
+
+
+def get_index(group: GroupBy, debug=True) -> int:
+    """Returns index of the max (only if one max value exists),
+    else returns the index of the median picks (manual pick are privileged)
+
+    Args:
+        group (GroupBy): picks cluster related to one station and phase (P|S)
+
+    Returns:
+        int: _description_
+    """
+    max_value = group["phase_score"].max()
+    max_value_indices = group[group["phase_score"] == max_value].index
+    if len(max_value_indices) == 1:
+        # only one absolute max
+        max_index = max_value_indices[0]
+        max_value = group.loc[max_index, "phase_score"]
+        if debug:
+            print(f"cluster nb picks {len(group)}, proba max {len(group)}: max_index={max_index} max_value={max_value}")
+        return max_index
+    else:
+        # multiple picks with max proba set
+        manual_df = group[group["phase_evaluation"] == "manual"]
+        if len(manual_df) >= 1:
+            # use only manual picks to get the preferred pick (based on median index)
+            median_index = (
+                manual_df["phase_score"].sort_values().index[len(manual_df) // 2]
+            )
+            median_value = manual_df.loc[median_index, "phase_score"]
+            if debug:
+                print(
+                    f"cluster nb picks {len(group)}, median manual {len(manual_df)}: median_index={median_index} median_value={median_value}"
+                )
+            return median_index
+        else:
+            median_index = group["phase_score"].sort_values().index[len(manual_df) // 2]
+            median_value = group.loc[median_index, "phase_score"]
+            if debug:
+                print(
+                    f"cluster nb picks {len(group)}, median other {len(group)}: median_index={median_index} median_value={median_value}"
+                )
+            return median_index
 
 
 def unload_too_close_picks_clustering(
     csv_file_in, csv_file_out, P_delta_time, S_delta_time
 ):
+
+    col_types = {
+        "station_id": "string",
+        "channel": "string",
+        "phase_type": "string",
+        "phase_time": "string",
+        "phase_score": "float64",
+        "phase_evaluation": "string",
+        "phase_method": "string",
+        "event_id": "string",
+        "agency": "string",
+    }
+
     print(f"Reading from {csv_file_in}.")
-    df = pd.read_csv(csv_file_in)
-    df.rename(
-        columns={
-            "seedid": "station_id",
-            "phasename": "phase_type",
-            "time": "phase_time",
-            "probability": "phase_score",
-        },
-        inplace=True,
-    )
-    # Keeps only network_code.station_code, move channel to
+    df = pd.read_csv(csv_file_in, dtype=col_types)
+
+    # Use only network_code.station_code
     df["channel"] = df["station_id"].map(lambda x: ".".join(x.split(".")[2:4]))
     df["station_id"] = df["station_id"].map(lambda x: ".".join(x.split(".")[:2]))
     df["phase_time"] = pd.to_datetime(df["phase_time"], utc=True)
@@ -33,7 +82,17 @@ def unload_too_close_picks_clustering(
 
     # empty dataframe
     results = pd.DataFrame(
-        columns=["station_id", "channel", "phase_type", "phase_time", "phase_score", "eventid", "agency"]
+        columns=[
+            "station_id",
+            "channel",
+            "phase_type",
+            "phase_time",
+            "phase_score",
+            "phase_evaluation",
+            "phase_method",
+            "event_id",
+            "agency",
+        ]
     )
 
     # run separately by phase type
@@ -72,7 +131,16 @@ def unload_too_close_picks_clustering(
             tmp_df["cluster"] = dbscan.fit_predict(tmp_df[["numeric_time"]])
 
             # keeps only the pick with the higher score
-            idx = tmp_df.groupby(["cluster"])["phase_score"].idxmax()
+            # idx_tmp = tmp_df.groupby(["cluster"])["phase_score"].idxmax()
+
+            # keeps the pick with the higher score or the median one.
+            # result = df.groupby('group').agg(index=('value', get_index))
+            # idx = tmp_df.groupby(["cluster"])["phase_score", "phase_evaluation"].apply(get_index)
+            # idx = tmp_df.groupby(["cluster"]).apply(lambda group: get_index(group[['phase_score', 'phase_evaluation']]))
+            idx = tmp_df.groupby(["cluster"])[
+                ["phase_score", "phase_evaluation"]
+            ].apply(get_index)
+
             tmp_df = tmp_df.loc[idx]
             tmp_df = tmp_df.drop(columns=["numeric_time", "cluster"])
             after = len(tmp_df)
@@ -94,7 +162,7 @@ def unload_too_close_picks_clustering(
 if __name__ == "__main__":
     # default logger
     logging.basicConfig(stream=sys.stdout, level=logging.DEBUG)
-    logger = logging.getLogger("dbclust")
+    logger = logging.getLogger("pick_preproc")
     logger.setLevel(logging.INFO)
 
     parser = argparse.ArgumentParser()
