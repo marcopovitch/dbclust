f300def79e88261d8edf63ea29f36594815c4889
Marc Grunberg <marc.grunberg@unistra.fr>
2025-08-05 15:00:49 +0200

    raise FileNotFoundError and exit when template could not be found

--- dbclust/localization.py
+++ dbclust/localization.py
@@ -905,6 +905,9 @@ class NllLoc(object):
                 cat = self.nll_localisation(
                     nll_obs_file, picks=picks_set, double_pass=self.double_pass
                 )
+            except FileNotFoundError as e:
+                logger.error(f"{e}")
+                sys.exit(1)
             except LocalizationError as e:
                 logger.warning(
                     f"{e} - trying with {fallback_loc_method} for {nll_obs_file}"
@@ -1419,13 +1422,13 @@ class NllLoc(object):
         Returns:
             None
         """
-        try
+        try:
             with open(templatefile) as file_:
                 template = Template(file_.read())
         except FileNotFoundError:
             logger.error(f"Template file {templatefile} not found.")
-            raise LocalizationError(
-                f"Template file {templatefile} not found."
+            raise FileNotFoundError(
+                f"Template file {templatefile} not found. Please check the path."
             )
             
         t = template.render(tags)
