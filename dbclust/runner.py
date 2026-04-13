13d842a26d459192e173eabe59f3c719acee8a33
Marc Grunberg <marc.grunberg@unistra.fr>
2026-04-13 13:30:18 +0200

    add limits to macos only

--- dbclust/runner.py
+++ dbclust/runner.py
@@ -22,11 +22,14 @@ import sys
 import warnings
 
 # Prevent thread explosion with numerical libraries (must be set before other imports)
-os.environ["OMP_NUM_THREADS"] = "1"
-os.environ["OPENBLAS_NUM_THREADS"] = "1"
-os.environ["MKL_NUM_THREADS"] = "1"
-os.environ["VECLIB_MAXIMUM_THREADS"] = "1"
-os.environ["NUMEXPR_NUM_THREADS"] = "1"
+# Restricted to macOS: on Linux/HPC these are intentionally left unset so that
+# numerical libraries can use all available cores.
+if sys.platform == "darwin":
+    os.environ["OMP_NUM_THREADS"] = "1"
+    os.environ["OPENBLAS_NUM_THREADS"] = "1"
+    os.environ["MKL_NUM_THREADS"] = "1"
+    os.environ["VECLIB_MAXIMUM_THREADS"] = "1"
+    os.environ["NUMEXPR_NUM_THREADS"] = "1"
 
 from dbclust.config import DBClustConfig
 from dbclust.core import dbclust
